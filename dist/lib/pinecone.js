ba4f88d602b076a01732aca67e73a552799b27a7
Kartik <kartik_a@mt.iitr.ac.in>
2024-05-15 04:02:57 +0530

    chneag

--- dist/lib/pinecone.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pinecone_1 = require("@pinecone-database/pinecone");
-const pc = new pinecone_1.Pinecone({
-    apiKey: 'ad1612ee-9b3f-4269-9e18-362ff724713d'
-});
-exports.default = pc;
--- dist/lib/redis.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Redis = require("ioredis");
-require('dotenv').config();
-const redis = new Redis(process.env.REDIS_URL || "");
-exports.default = redis;
